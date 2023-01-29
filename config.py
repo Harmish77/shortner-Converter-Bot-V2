4682c57a2c81efe84cb8942a563c9e895cee3223
Harmish77 <107766922+Harmish77@users.noreply.github.com>
2023-01-29 12:26:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,5 +21,5 @@ ADMINS.append(Id Owned Id)
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001240066432")) 
 UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "main_movie_hub") # For Force Subscription
 BROADCPAST_AS_COPY = os.environ.get('BROADCAST_AS_COPY', "True") # true if forward should be avoided
-WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", '') # image when someone hit /start # image when someone hit /start
+WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", 'https://telegra.ph/file/dc652fe85ec8b8ae9a1d7.jpg') # image when someone hit /start # image when someone hit /start
 LINK_BYPASS = "False"
