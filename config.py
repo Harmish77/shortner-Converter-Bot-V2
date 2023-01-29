e9c4993d95b0834b83be3f7d0064a78bcbb54254
Harmish77 <107766922+Harmish77@users.noreply.github.com>
2023-01-29 11:47:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,10 +7,10 @@ load_dotenv()
 
 
 # Mandatory variables for the bot to start
-API_ID = int(os.getenv("API_ID", "Your Api Id"))
-API_HASH = os.environ.get("API_HASH", "Your Api Hash")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "Bot Token")
-ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("Owner Id")] if os.environ.get("ADMINS") else []
+API_ID = int(os.getenv("API_ID", "1240066432"))
+API_HASH = os.environ.get("API_HASH", "113378543a8c43310ff96d22cc95061f")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6047286645:AAG5Iv_LCWaHGPFG4_JXtyiRA_MRtVm2za4")
+ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("1240066432")] if os.environ.get("ADMINS") else []
 ADMIN = ADMINS
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "Db Name")
 DATABASE_URL = os.getenv("DATABASE_URL", "Monfo url") 
